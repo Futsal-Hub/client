c0d6754f590c892dd87fdb784e17ad9f03bf93cf
fajarrrX <fajarrr30@gmail.com>
2021-01-22 13:16:58 +0700

    first initilization: 1. expo init 2. install all depedencies such as native-base, react-navigation 3. create skeleton file and folder

---
+++ src/screens/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "native-base";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Text>LandingPage</Text>
+    </div>
+  );
+};
+
+export default LandingPage;
