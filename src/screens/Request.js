4d614d076428a0760c4a1f4324a942a686db2de6
sangsangga <wandaputrasangga@gmail.com>
2021-01-27 15:16:37 +0700

    remove button in request screen

--- src/screens/Request.js
+++ src/screens/Request.js
@@ -29,9 +29,6 @@ const Request = () => {
     <Container>
       <Content>
         <Text>List Request</Text>
-        <Button onPress={() => dispatch(getReceivedRequest())}>
-          <Text>GET reqest</Text>
-        </Button>
         <Card>
           {receivedRequest.map((item) => {
             return (
