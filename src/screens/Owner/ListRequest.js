22769d02f81b1d4521d25057e6299ccb09bb79f7
arifmr <arifmr@pop-os.localdomain>
2021-01-28 22:37:45 +0700

    styling

--- src/screens/Owner/ListRequest.js
+++ src/screens/Owner/ListRequest.js
@@ -76,40 +76,40 @@ const ListRequest = ({navigation}) => {
               />
             </TouchableOpacity>
         </Header>
-        <Card style={{marginLeft: 10, marginRight: 10, marginTop: 10}}>
+        <View style={{marginLeft: 10, marginRight: 10, marginTop: 10}}>
           {
             listBooking.map(booking => {
               console.log(booking)
               return (
-                <CardItem bordered key={booking._id} style={{ marginTop: 10, flex: 1, flexDirection: 'row'}}>
-                    <Image
-                      source={{uri: booking.court.photos}}
-                      style={{width: 150, height: 120, }}
-                    />
-                    <Body style={{marginLeft: 30}}>
-                      <Text style={{fontSize: 20, color: '#EF7911'}}>{booking.court.name}</Text>
-                      <View style={{flex:1, flexDirection: 'row', marginTop: 5}}>
-                        <AntDesign name="user" size={20} color="black" />
-                        <Text style={{fontSize: 12, marginLeft: 10}}>{booking.host.username}</Text>
-                      </View>
-                      <View style={{flex:1, flexDirection: 'row', marginTop: 5}}>
-                        <AntDesign name="calendar" size={20} color="black" />
-                        <Text style={{fontSize: 12, marginLeft: 10}}>{booking.date.date}</Text>
-                      </View>
-                      <View style={{flex:1, flexDirection: 'row', marginTop: 5}}>
-                        <AntDesign name="clockcircleo" size={20} color="black" />
-                        <Text style={{fontSize: 12, marginLeft: 10}}>{booking.date.duration} Hour</Text>
-                      </View>
-                      <View style={{flex: 1, flexDirection: 'row', marginTop: 5}}>
-                        <AntDesign name="checkcircle" size={30} color="blue" onPress={() => decisionHandler(booking, 'accepted')} />
-                        <AntDesign style={{marginLeft: 50}} name="closecircle" size={30} color="red" onPress={() => decisionHandler(booking, 'rejected')}/>
-                      </View>
-                    </Body>
-                </CardItem>
+                <Card bordered key={booking._id} style={{ marginTop: 10, marginBottom: 10, paddingTop: 10, paddingBottom:10, flex: 1, flexDirection: 'row'}}>
+                  <Image
+                    source={{uri: booking.court.photos}}
+                    style={{width: 150, height: 120, marginTop: 'auto', marginBottom: 'auto', marginLeft: 10, marginRight: 'auto'}}
+                  />
+                  <Body style={{marginLeft: 30}}>
+                    <Text style={{fontSize: 20, color: '#EF7911'}}>{booking.court.name}</Text>
+                    <View style={{flex:1, flexDirection: 'row', marginTop: 5}}>
+                      <AntDesign style={{right: 40}} name="user" size={20} color="black" />
+                      <Text style={{fontSize: 12, right: 30}}>{booking.host.username}</Text>
+                    </View>
+                    <View style={{flex:1, flexDirection: 'row', marginTop: 5}}>
+                      <AntDesign style={{left: 1}} name="calendar" size={20} color="black" />
+                      <Text style={{fontSize: 12, marginLeft: 10}}>{booking.date.date}</Text>
+                    </View>
+                    <View style={{flex:1, flexDirection: 'row', marginTop: 5}}>
+                      <AntDesign style={{right: 31}} name="clockcircleo" size={20} color="black" />
+                      <Text style={{fontSize: 12, right: 20}}>{booking.date.duration} Hour</Text>
+                    </View>
+                    <View style={{flex: 1, flexDirection: 'row', marginTop: 10}}>
+                      <AntDesign name="checkcircle" size={30} color="blue" onPress={() => decisionHandler(booking, 'accepted')} />
+                      <AntDesign style={{marginLeft: 50}} name="closecircle" size={30} color="red" onPress={() => decisionHandler(booking, 'rejected')}/>
+                    </View>
+                  </Body>
+                </Card>
               )
             })
           }
-        </Card>
+        </View>
       </Content>
     </Container>
   )
